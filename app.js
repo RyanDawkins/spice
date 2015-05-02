6c07054a7ce811eefdb151bcfb6ff51f4a74ef11
Ryan Dawkins <ryan@ryanddawkins.com>
2015-05-02 09:45:44 -0500

    Can now receive requests and find the correct router

--- app.js
+++ app.js
@@ -3,18 +3,45 @@ var config = require('config');
 var entry = require('entry');
 var www = require('www');
 var url = require('url');
-var routes = require('');
+var fs = require('fs');
 
 var port = config.server.port;
 var host = config.server.host;
 
-http.createServer(function receive(req, res) {
+// This will be the callback for finishing the load routes.
+var startServer = function(entry) {
+  http.createServer(function receive(req, res) {
+    var urlData = url.parse(req.url);
+    console.log("Request: "+req.method+" "+urlData.pathname);
 
-  var urlData = url.parse(req.url);
-  console.log("Request to: "+urlData.pathname);
+    var pathSplit = urlData.pathname.split(".");
+    var prefix = pathSplit[0];
+    var suffix = pathSplit.length > 1
+      ? pathSplit[1]
+      : "json";
 
-  entry.getPathCallback(req.method, urlData.pathname)(www.getWriter());
+    console.log(entry.getCallbacks());
 
-}).listen(port, host);
+    var callback = entry.getPathCallback(req.method, prefix);
+    if(callback) {
+      callback(req, res, www.getWriter(suffix));
+    } else {
+      www.notFound(req, res);
+    }
 
-console.log('Server running at http://'+host+':'+port);
+  }).listen(port, host);
+  console.log('Server running at http://'+host+':'+port);
+}
+
+var loadRoutes = function() {
+  fs.readdir('./routes', function(err, files) {
+    for(var i = 0; i < files.length; i++) {
+      var callbacks = require("./routes/"+files[i]).entry.callbacks;
+      entry.extend(callbacks);
+    }
+
+    startServer(entry);
+  });
+};
+
+loadRoutes();
